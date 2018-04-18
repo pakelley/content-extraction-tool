7e5576ca1491085414b4d91fed752bbdbe68ce6f
Patrick Kelley <pkelley@bombora.com>
2018-04-18 09:04:39 -0700

    Refactor/reformat widget

--- utils.py
+++ utils.py
@@ -1,10 +1,12 @@
 import os
 import re
+import logging
 import lxml
 import pandas as pd
 from tqdm import tqdm_notebook as tqdm
 import ipywidgets as widgets
 from IPython.display import HTML, display
+from traitlets import Unicode
 
 from dragnet import Extractor
 from dragnet.blocks import simple_tokenizer as tokenizer
@@ -12,6 +14,9 @@ from dragnet.compat import train_test_split
 from dragnet.data_processing import prepare_all_data, extract_all_gold_standard_data, get_filenames
 from dragnet.util import evaluation_metrics
 import justext
+
+log = logging.getLogger()
+log.setLevel('ERROR')
 
 def init_cleaner():
     cleaner = lxml.html.clean.Cleaner()
@@ -42,36 +47,25 @@ def read_dragnet_data(data_dir, to_extract='content'):
     'fileroot', 'doc', 'labels', 'weights', and 'tokens' for each document
     """
     # read data
-    filenames = [re.sub(r'\.html', '', fname)
-                 for fname in list(get_filenames(os.path.join(data_dir, 'HTML')))]
+    urls_df = pd.read_csv(os.path.join(data_dir, 'URLs_summary.csv'))
+    urls_df['fileroot'] = urls_df['html_file_name'].apply(lambda fname: re.sub(r'\.html', '', fname))
     extract_all_gold_standard_data(data_dir)
     data = prepare_all_data(data_dir)
     # parse data
-    full_df = make_dragnet_df(list(zip(data, filenames)), 'content')
-    del data
-
-    return full_df
-
-def make_dragnet_df(data_and_filenames, to_extract):
-    data = [item[0] for item in data_and_filenames]
-    filenames = [item[1] for item in data_and_filenames]
+    data_df = pd.DataFrame({'corrected_paths': list(get_filenames(os.path.join(data_dir, 'Corrected')))})
+    data_df['fileroot'] = data_df['corrected_paths'].apply(lambda fname: re.sub(r'\.html.corrected.txt', '', fname))
+    data_df.drop(columns='corrected_paths')
+    
+    data_df['doc'] = [item[0] for item in data]
     tmp_extractor = Extractor(to_extract=to_extract)
-    del data_and_filenames
-    docs = [item[0] for item in data]
     labels_weights = [tmp_extractor._get_labels_and_weights(item[1], item[2]) for item in data]
-    labels = [item[0] for item in labels_weights]
-    weights = [item[1] for item in labels_weights]
-    tokens = get_gs_tokens(data, tmp_extractor.to_extract)
-    del data
-    del labels_weights
+    data_df['labels'] = [item[0] for item in labels_weights]
+    data_df['weights'] = [item[1] for item in labels_weights]
+    data_df['tokens'] = get_gs_tokens(data, to_extract)
+    
+    data_df = data_df.join(urls_df.set_index('fileroot'), on='fileroot').rename(columns={'URL': 'url'})
 
-    return pd.DataFrame({
-        'filename': filenames,
-        'doc': docs,
-        'labels': labels,
-        'weights': weights,
-        'tokens': tokens
-    })
+    return data_df
 
 def get_gs_content(extractor, doc, labels):
     """util to get labeled content from a doc"""
@@ -117,7 +111,8 @@ def extraction_comparison(base_ext, comp_ext, df, npartitions=1):
                    for predicted, expected, weight in zip(comp_tokens, expected_tokens, df['weights'])]
 
     return pd.DataFrame({
-        'fileroot': df['filename'],
+        'fileroot': df['fileroot'],
+        'url': df['url'],
         'test_data': df['doc'],
         'test_labels': df['labels'],
         'test_weights': df['weights'],
@@ -131,86 +126,169 @@ def extraction_comparison(base_ext, comp_ext, df, npartitions=1):
         'base_f1': [score[2] for score in base_scores],
         'comp_f1': [score[2] for score in comp_scores]
     })
-def content_extract_comparison_widget(df):
-    """creates a widget for analyzing content extraction"""
-    def print_expected(df, idx):
-        print(df['expected_content'].iloc[idx])
 
-    def print_dragnet(df, idx):
-        print('f1: ', df['base_f1'].iloc[idx])
-        print(df['base_content'].iloc[idx])
 
-    def print_justext(df, idx):
-        print('f1: ', df['comp_f1'].iloc[idx])
-        print(df['comp_content'].iloc[idx])
+class FancySlider(widgets.HBox):
+    def __init__(self, max=20, value=0):
+        _view_name = Unicode('FancySlider').tag(sync=True)
         
-    def print_raw_html(df, idx):
-        print(df['test_data'].iloc[idx])
+        self.slider = widgets.IntSlider(value=value, min=0, max=max, 
+            continuous_update=False, readout=False, step=1,
+            layout=widgets.Layout(width='15em'))
+        
+        button_layout = widgets.Layout(width='2.5em', height='2.5em')
+        self.plus_button = widgets.Button(icon='plus', button_style='', layout=button_layout)
+        self.minus_button = widgets.Button(icon='minus', button_style='', layout=button_layout)
+        
+        self.plus_button.on_click(self.increment_slider)
+        self.minus_button.on_click(self.decrement_slider)
 
-    slider = widgets.IntSlider(min=0, max=df.count()['base_content']-1, step=1, value=0, continuous_update=False)
+        self.textbox = widgets.Text(str(self.value), layout=widgets.Layout(width='2.2em'))
+        self.textbox.observe(self.update_slider, 'value')
+        self.slider.observe(self.update_textbox, 'value')
+        
+        super(FancySlider, self).__init__([self.minus_button, self.plus_button, self.slider, self.textbox])
 
-    # build accordion for content
-    expected_interact = widgets.interactive_output(print_expected, {'df': widgets.fixed(df), 'idx': slider})
-    dragnet_interact = widgets.interactive_output(print_dragnet, {'df': widgets.fixed(df), 'idx': slider})
-    justext_interact = widgets.interactive_output(print_justext, {'df': widgets.fixed(df), 'idx': slider})
-
-    accordion = widgets.Accordion(children=[expected_interact, dragnet_interact, justext_interact])
-    accordion.set_title(0, 'Expected')
-    accordion.set_title(1, 'Dragnet')
-    accordion.set_title(2, 'Justext')
-
-    # build HTML view
-    cleaner = init_cleaner()
-    def show_html(df, idx):
-        html_str = df['test_data'].iloc[idx]
-        clean_html_str = cleaner.clean_html(html_str)
-        display(HTML(clean_html_str))
-    html_view = widgets.interactive_output(show_html, {'df': widgets.fixed(df), 'idx': slider})
+    @property
+    def value(self):
+        return self.slider.value
     
-    raw_html_view = widgets.interactive_output(print_raw_html, {'df': widgets.fixed(df), 'idx': slider})
+    @value.setter
+    def value(self, val):
+        self.slider.value = val
 
-    # make file label
-    file_label = widgets.Label(df['fileroot'].iloc[slider.value])
+    def increment_slider(self, *args):
+        self.value += 1
+        self.update_textbox()
 
-    def update_file_label(*args):
-        file_label.value = df['fileroot'].iloc[slider.value]
-    slider.observe(update_file_label, 'value')
+    def decrement_slider(self, *args):
+        self.value -= 1
+        self.update_textbox()
 
-    # build error mode labels
-    if 'error_modes' not in df.columns:
-        df['error_modes'] = pd.Series([''] * len(df.index), index=df.index)
+    def update_slider(self, *args):
+        if self.textbox.value is not '':
+        self.value = int(self.textbox.value)
 
-    em_text = widgets.Text(df['error_modes'].iloc[slider.value])
-    def update_em(*args):
-        df['error_modes'].iloc[slider.value] = em_text.value
-    em_text.observe(update_em, 'value')
+    def update_textbox(self, *args):
+        self.textbox.value = str(self.value)
+        
+    def observe(self, *args, **kwargs):
+        if 'slider' in dir(self):
+            self.slider.observe(*args, **kwargs)
 
-    def update_text(*args):
-        em_text.value = df['error_modes'].iloc[slider.value]
-    slider.observe(update_text, 'value')
+class ExtractionAccordion(widgets.Accordion):
+    def __init__(self, slider, df):
+        # build accordion for content
+        self.df = df
+        self.expected = widgets.interactive_output(self.print_expected, 
+                                                       {'idx': slider})
+        self.dragnet = widgets.interactive_output(self.print_dragnet, 
+                                                      {'idx': slider})
+        self.justext = widgets.interactive_output(self.print_justext, 
+                                                      {'idx': slider})
+        
+        (super(ExtractionAccordion, self).__init__(
+            children=[self.expected, self.dragnet, self.justext]))
+        
+        self.set_title(0, 'Expected')
+        self.set_title(1, 'Dragnet')
+        self.set_title(2, 'Justext')
 
-    # make content entry field
-    content_text = widgets.Textarea(df['labeled_content'].iloc[slider.value],
-                                    layout=widgets.Layout(width='auto'))
+        accordion = widgets.Accordion()
+        
+    def print_expected(self, idx):
+        print(self.df['expected_content'].iloc[idx])
 
-    def update_content(*args):
-        df['labeled_content'].iloc[slider.value] = content_text.value
-    content_text.observe(update_content, 'value')
+    def print_dragnet(self, idx):
+        print('f1: ', self.df['base_f1'].iloc[idx])
+        print(self.df['base_content'].iloc[idx])
 
-    def update_content_text(*args):
-        content_text.value = df['labeled_content'].iloc[slider.value]
-    slider.observe(update_content_text, 'value')
+    def print_justext(self, idx):
+        print('f1: ', self.df['comp_f1'].iloc[idx])
+        print(self.df['comp_content'].iloc[idx])
 
-    # collect into tabs
-    tab_nest = widgets.Tab()
-    tab_nest.children = [accordion, html_view, raw_html_view, content_text]
-    tab_nest.set_title(0, 'Extracted Content')
-    tab_nest.set_title(1, 'Rendered HTML')
-    tab_nest.set_title(2, 'Raw HTML')
-    tab_nest.set_title(3, 'Label Content')
+class ContentExtractComparisonWidget(widgets.Tab):
+    """creates a widget for analyzing content extraction"""
+    def __init__(self, df):
+        self.slider = FancySlider(max=df.count()['base_content']-1, value=0)
+        self.df = df
+        
+        self.accordion = ExtractionAccordion(self.slider, self.df)
+        
+        # build HTML views
+        self.cleaner = init_cleaner() # TODO
+        self.html_view = widgets.interactive_output(self.show_html, {'idx': self.slider})
+        self.raw_html_view = widgets.interactive_output(lambda idx: print(df['test_data'].iloc[idx]), {'idx': self.slider})
+        self.file_label = widgets.interactive_output(self.get_file_label, {'idx': self.slider})
+        self.url_view = widgets.interactive_output(self.show_url, {'idx': self.slider})
 
-    # final display
-    return widgets.VBox([widgets.HBox([file_label, slider, widgets.Label('Error Mode:'), em_text]), tab_nest])
+        # build error mode labels
+        if 'reviewed' not in df.columns:
+            df['reviewed'] = pd.Series([False] * len(df.index), index=df.index)
+
+            
+        self.reviewed_cbox = widgets.ToggleButton(
+            value=bool(df.loc[self.slider.value, 'reviewed']),
+            description='Reviewed',
+            layout=widgets.Layout(width='8em')
+        )
+        self.stylize_reviewed_cbox()
+            
+        self.reviewed_cbox.observe(self.update_reviewed_df, 'value')
+        self.reviewed_cbox.observe(self.stylize_reviewed_cbox, 'value')
+        self.slider.observe(self.update_reviewed_cbox, 'value')
+
+        # make content entry field
+        self.content_text = widgets.Textarea(df['labeled_content'].iloc[self.slider.value],
+                                        layout=widgets.Layout(width='auto'))
+
+
+        self.content_text.observe(self.update_content, 'value')
+        self.slider.observe(self.update_content_text, 'value')
+
+        # collect into tabs
+        (super(ContentExtractComparisonWidget, self).__init__([self.accordion, self.html_view, self.raw_html_view, self.content_text]))
+        self.set_title(0, 'Extracted Content')
+        self.set_title(1, 'Rendered HTML')
+        self.set_title(2, 'Raw HTML')
+        self.set_title(3, 'Label Content')
+    
+    def show_html(self, idx):
+        html_str = self.df.loc[idx, 'test_data']
+        clean_html_str = self.cleaner.clean_html(html_str)
+        display(HTML(clean_html_str))
+        
+    def show_url(self, idx):
+        display(HTML("<a href=\"{url}\">{url}</a>".format(url=self.df.loc[idx, 'url'])))
+        
+    def get_file_label(self, idx):
+        display(widgets.Label('Fileroot:  {fileroot}'.format(fileroot=self.df.loc[idx, 'fileroot'])))
+    
+    def update_reviewed_df(self, *args):
+        self.df.loc[self.slider.value, 'reviewed'] = self.reviewed_cbox.value
+        
+    def update_reviewed_cbox(self, *args):
+        self.reviewed_cbox.value = bool(self.df.loc[self.slider.value, 'reviewed'])
+        self.stylize_reviewed_cbox()
+        
+    def stylize_reviewed_cbox(self, *args):
+        self.reviewed_cbox.icon = 'check' if self.reviewed_cbox.value else 'times'
+        self.reviewed_cbox.button_style = 'success' if self.reviewed_cbox.value else ''
+        
+    def update_content(self, *args):
+        self.df.loc[self.slider.value, 'labeled_content'] = self.content_text.value
+        
+    def update_content_text(self, *args):
+        self.content_text.value = self.df.loc[self.slider.value, 'labeled_content']
+        
+    def _ipython_display_(self, *args):
+        composed_widget = widgets.VBox([
+            widgets.HBox([self.slider, self.reviewed_cbox]),
+            widgets.HBox([widgets.HBox([widgets.Label('URL:  '), self.url_view]),
+                          self.file_label]),
+            widgets.Label("Content/comments separator: !@#$%^&*() COMMENTS"), 
+            self])
+        return composed_widget._ipython_display_(*args)
 
 
 class JustextWrapper():
